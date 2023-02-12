36d6e247b0d24ca0bf446e61a3ccc26520b74833
Evan Tann <os@evantann.com>
2023-02-11 18:36:07 -0800

    rm binary on deploy proxy

--- deploy_proxy.sh
+++ deploy_proxy.sh
@@ -5,4 +5,4 @@ CGO_ENABLED=0 go build -o proxy/proxy ./cmd/proxy
 go install ./cmd/yeoman
 cp ~/.config/gcloud/application_default_credentials.json proxy/
 yeoman -http service deploy proxy
-rm proxy/application_default_credentials.json
+rm proxy/proxy proxy/application_default_credentials.json
